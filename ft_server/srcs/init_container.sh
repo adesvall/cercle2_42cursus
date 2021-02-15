133121bfb2e1b84cd7a9d7b2901afc1b09811ef5
adesvall <adesvall@student.42.fr>
2021-02-15 15:34:27 +0100

    ft_server_soon

--- ft_server/srcs/init_container.sh
+++ ft_server/srcs/init_container.sh
@@ -5,16 +5,16 @@ chown -R www-data /var/www/*
 chmod -R 755 /var/www/*
 
 # Generate website folder
-mkdir /var/www/siteinternet && touch /var/www/siteinternet/index.php
-echo "<?php phpinfo(); ?>" >> /var/www/siteinternet/index.php
+mkdir /var/www/monsupersite && touch /var/www/monsupersite/index.php
+echo "<?php phpinfo(); ?>" >> /var/www/monsupersite/index.php
 
 # SSL
 mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/siteinternet.pem -keyout /etc/nginx/ssl/siteinternet.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=adesvall/CN=siteinternet"
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/monsupersite.pem -keyout /etc/nginx/ssl/monsupersite.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=adesvall/CN=monsupersite"
 
 # Config NGINX
-mv ./tmp/nginx-conf /etc/nginx/sites-available/siteinternet
-ln -s /etc/nginx/sites-available/siteinternet /etc/nginx/sites-enabled/siteinternet
+mv ./tmp/nginx-conf /etc/nginx/sites-available/monsupersite
+ln -s /etc/nginx/sites-available/monsupersite /etc/nginx/sites-enabled/monsupersite
 rm -rf /etc/nginx/sites-enabled/default
 
 # Config MYSQL
@@ -24,17 +24,24 @@ echo "update mysql.user set plugin='mysql_native_password' where user='root';" |
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
 # DL phpmyadmin
-mkdir /var/www/siteinternet/phpmyadmin
+mkdir /var/www/monsupersite/phpmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
-tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz --strip-components 1 -C /var/www/siteinternet/phpmyadmin
-mv ./tmp/phpmyadmin.inc.php /var/www/siteinternet/phpmyadmin/config.inc.php
+tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz --strip-components 1 -C /var/www/monsupersite/phpmyadmin
+mv ./tmp/phpmyadmin.inc.php /var/www/monsupersite/phpmyadmin/config.inc.php
 
 # DL wordpress
 cd /tmp/
 wget -c https://wordpress.org/latest.tar.gz
 tar -xvzf latest.tar.gz
-mv wordpress/ /var/www/siteinternet
-mv /tmp/wp-config.php /var/www/siteinternet/wordpress
+mv wordpress/ /var/www/monsupersite
+mv /tmp/wp-config.php /var/www/monsupersite/wordpress/
+
+# Config wordpress
+cd /var/www/monsupersite/wordpress
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+#wp core install --url='' --title='LE TITRE' --admin_user='adesvall' --admin_email='adesvall@42.student.fr' --skip-email --allow-root
 
 service php7.3-fpm start
 service nginx start
